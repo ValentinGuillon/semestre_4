87f0367a30726e98004353857a65eadf1d26a708
Valentin_G <valentin.guillon93430@gmail.com>
2023-04-16 16:21:18 +0200

    ASD2, tp5: prog fini

--- 6_algo-struct-donnees-2/tp5/src/dijkstra.c
+++ 6_algo-struct-donnees-2/tp5/src/dijkstra.c
@@ -5,6 +5,7 @@
 
 
 void dijkstra(int **mat, int size, int node, int **paths) {
+    node -= 1;
     for (int i = 0; i < size; i++) {
         if (i == node)
             paths[i][0] = 0;
@@ -16,45 +17,30 @@ void dijkstra(int **mat, int size, int node, int **paths) {
 
 
 
-
 //détermine le chemin le plus court entre "node_a" et "node_b"
 void fastest_path(int **mat, int size, int node_a, int node_b, int *path){
     int distances[size]; //distance minimal entre "node_a" et les autres
-    int visited[size]; //état visité(1) ou non(0) de chaque noeud de la matrice (ex: noeud 4 <=> index 3)
-    tuple predecesseurs[size]; //a:predecesseurs, b:distance from a. Noeud précédent l'observation de chaque noeud
+    int visited[size]; //état visité(1) ou non(0) de chaque noeud de la matrice
+    tuple predecesseurs[size]; //Noeud précédent l'observation de chaque noeud
     int distance_from_a = 0; //distance entre le noeud de départ, et le noeud observé
 
-    int current_node = node_a; //noeud observé
     //initialiser les tableaux
     fill_with(visited, size, 0); visited[node_a] = 1;
     fill_with(distances, size, INF); distances[node_a] = 0;
-    // fill_with(predecesseurs, size, -1); predecesseurs[node_a] = INF;
+
     for (int i = 0; i < size; i++) {
-        predecesseurs[i].a = -1;
-        predecesseurs[i].b = INF;
+        predecesseurs[i].node = -1;
+        predecesseurs[i].dist = INF;
     }
+    predecesseurs[node_a].node = -2; //init to -2 to use it as clause condition when backtrack the path
+    predecesseurs[node_a].dist = 0;
 
 
-    int input;
-    int loop_without_operation = 0;
+
+    int current_node = node_a; //noeud observé
+
+    //enfin, l'algo de mort mdr
     while (!all_visited(visited, size)) {
-        if (loop_without_operation > 10000)
-            break;
-        printf("------------loop %d\n", loop_without_operation);
-        printf("current node:    %d\n", current_node+1);
-        printf("distance_from_a: %d\n", distance_from_a);
-        printf("distances:       "); print_tab(distances, size);
-        printf("visited:         "); print_tab(visited, size);
-        printf("predecesseurs:   "); //print_tab(predecesseurs, size);
-        for (int i = 0; i < size-1; i++)
-            printf("%d(%d,%d), ", i, predecesseurs[i].a, predecesseurs[i].b);
-        printf("%d(%d,%d)\n", size-1, predecesseurs[size-1].a, predecesseurs[size-1].b);
-
-        // scanf("%d", &input);
-
-        // if (loop_without_operation <= size)
-        //     break;
-
 
         for (int i = 0; i < size; i++) {
             //ignore visited node
@@ -74,40 +60,29 @@ void fastest_path(int **mat, int size, int node_a, int node_b, int *path){
 
             if (distances[i] > mat[current_node][i] + distance_from_a) {
                 distances[i] = mat[current_node][i] + distance_from_a;
-                predecesseurs[i].a = current_node;
-                predecesseurs[i].b = distances[i];
+                predecesseurs[i].node = current_node;
+                predecesseurs[i].dist = distances[i];
             }
         }
 
-
         int temp = index_of_the_smallest_no_visited(distances, visited, size);
 
-        // distance_from_a = distance_from_start(mat, predecesseurs, temp);
-        distance_from_a = predecesseurs[temp].b;
+        distance_from_a = predecesseurs[temp].dist;
         current_node = temp;
         
         visited[current_node] = 1;
-
-
-        loop_without_operation++;
     }
 
 
-    path[0] = distances[node_b];
-    // for (int i = 1; i < size + 1; i++)
-    //     path[i] = i*2;
-
-    // enregistrement de la distance minimal, et du chemin
+    // enregistrement de la distance minimal
     path[0] = distances[node_b];
 
-    int to = node_b, from = predecesseurs[to].a;
-    for (int i = 1; to != node_a && to != -1; i++) {
-        path[i] = to;
-        printf("%d\n", path[i]);
-        to = predecesseurs[from].a;
-        from = predecesseurs[to].a;
+    // find path
+    for (int i = 1, to = node_b, from; to != -2; i++) {
+        path[i] = to+1;
+        from = to;
+        to = predecesseurs[from].node;
     }
-
 }
 
 
@@ -115,6 +90,24 @@ void fastest_path(int **mat, int size, int node_a, int node_b, int *path){
 
 
 
+
+void afficher_trajets(int **trajets, int size, int start_node) {
+    start_node -= 1;
+
+    for (int i = 0; i < size; i++) {
+        if (i == start_node)
+            continue;
+        
+        printf("Distance de %d à %d = %d\n", start_node+1, i+1, trajets[i][0]);
+        printf("Trajet: ");
+        for (int j = size; j > 1; j--) {
+            if (trajets[i][j] == -1)
+                continue;
+            printf("%d->", trajets[i][j]);
+        }
+        printf("%d\n\n", trajets[i][1]);
+    }
+}
 
 
 
@@ -138,19 +131,19 @@ int all_visited(int *tab, int size) {
     return 1;
 }
 
-//return the smallest no-null value
-int smallest(int *tab, int size) {
-    int minimal = INF;
+// //return the smallest no-null value
+// int smallest(int *tab, int size) {
+//     int minimal = INF;
 
-    for (int i = 0; i < size; i++) {
-        //ignore nul value
-        if (tab[i] == 0)
-            continue;
-        if (tab[i] < minimal)
-            minimal = tab[i];
-    }
-    return minimal;
-}
+//     for (int i = 0; i < size; i++) {
+//         //ignore nul value
+//         if (tab[i] == 0)
+//             continue;
+//         if (tab[i] < minimal)
+//             minimal = tab[i];
+//     }
+//     return minimal;
+// }
 
 //return the index containing the smallest no-null value
 int index_of_the_smallest_no_visited(int *tab, int *visited, int size) {
@@ -182,26 +175,24 @@ int index_of_the_smallest_no_visited(int *tab, int *visited, int size) {
 }
 
 
-void print_tab(int *tab, int size) {
-    for (int i = 0; i < size-1; i++)
-        printf("%d, ", tab[i]);
-    printf("%d\n", tab[size-1]);
-}
+// void print_tab(int *tab, int size) {
+//     for (int i = 0; i < size-1; i++)
+//         printf("%d, ", tab[i]);
+//     printf("%d\n", tab[size-1]);
+// }
 
 
 
-int distance_from_start(int **mat, tuple *predecesseurs, int to) {
-    int distance = 0;
+// int distance_from_start(int **mat, tuple *predecesseurs, int to) {
+//     int distance = 0;
 
-    int from = predecesseurs[to].a;
-    while(to != 2 || to != -1) {
-        printf("from=%d, to=%d\n", from, to);
-        distance += mat[from][to];
-        to = predecesseurs[from].a;
-        from = predecesseurs[to].a;
-    }
+//     int from = predecesseurs[to].node;
+//     while(to != 2 || to != -1) {
+//         printf("from=%d, to=%d\n", from, to);
+//         distance += mat[from][to];
+//         to = predecesseurs[from].node;
+//         from = predecesseurs[to].node;
+//     }
 
-    return distance;
-}
-
-
+//     return distance;
+// }
--- 6_algo-struct-donnees-2/tp5/tp5.c
+++ 6_algo-struct-donnees-2/tp5/tp5.c
@@ -1,3 +1,6 @@
+/* programme déterminant le chemin le plus rapide entre un noeud donné et tous les autres d'une matrice
+ * en utilisant le'algorithme de Dijkstra
+ */
 
 #include<dijkstra.h>
 #include<stdio.h>
@@ -7,77 +10,100 @@
 #define SIZE 7 //nombre de noeud de la matrice
 
 
-int main(void) {
-    //réseau de métro parisiens sur 7 gares principales
-    int matrice[SIZE][SIZE] = {{0,   INF, INF, INF, INF, 12,  5},
-                               {INF, 0,   2,   INF, INF, INF, 7},
-                               {INF, 2,   0,   13,  INF, INF, 10},
-                               {INF, INF, 13,  0,   INF, INF, INF},
-                               {INF, INF, INF, INF, 0,   INF, 2},
-                               {12,  INF, INF, INF, INF, 0,   12},
-                               {5,   7,   10,  INF, 2,   12,  0}};
-
-
-    /*
-    //pareil, mais avec une ligne ne circulant pas dans un sens
-    int matrice[SIZE][SIZE] = {{0,   INF, INF, INF, INF, 12,  5},
-                               {INF, 0,   2,   INF, INF, INF, 7},
-                               {INF, 2,   0,   13,  INF, INF, INF},
-                               {INF, INF, 13,  0,   INF, INF, INF},
-                               {INF, INF, INF, INF, 0,   INF, 2},
-                               {12,  INF, INF, INF, INF, 0,   12},
-                               {5,   7,   10,  INF, 2,   INF, 0}};
-    */
-
-    int **mat = malloc(SIZE * sizeof(int*));
-    for (int i = 0; i < SIZE; i++)
-        mat[i] = malloc(SIZE * sizeof(int*));
-
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++)
-            mat[i][j] = matrice[i][j];
+int main(int argc, char** argv) {
+    // arrêt du programme si on ne donne pas d'argument à l'exécution
+    if (argc == 1) {
+        printf("Argument is missing: must give at least one.\n");
+        printf(" 1: metro parisien\n");
+        printf(" 2: metro parisien sans ligne 4 dans un sens\n");
+        return 0;
     }
 
-    // for (int i = 0; i < SIZE; i++){
-    //     for (int j = 0; j < SIZE; j++) {
-    //         printf("matrice[%d][%d] = %d\n", i, j, matrice[i][j]);
-    //         printf("mat    [%d][%d] = %d\n", i, j, mat[i][j]);
-    //     }
-    // }
+    // arrêt du programme si l'argument donné n'est pas correcte
+    if (argv[1][0] != '1' && argv[1][0] != '2') {
+        printf("Wrong argument.\n");
+        printf(" 1: metro parisien\n");
+        printf(" 2: metro parisien sans ligne 4 dans un sens\n");
+        return 0;
+    }
 
 
 
+    // réseau de métro parisiens sur 7 gares principales
+    int metro_parisien[SIZE][SIZE] = {
+        {0,   INF, INF, INF, INF, 12,  5},
+        {INF, 0,   2,   INF, INF, INF, 7},
+        {INF, 2,   0,   13,  INF, INF, 10},
+        {INF, INF, 13,  0,   INF, INF, INF},
+        {INF, INF, INF, INF, 0,   INF, 2},
+        {12,  INF, INF, INF, INF, 0,   12},
+        {5,   7,   10,  INF, 2,   12,  0}
+    };
+    
+    // pareil, mais avec une ligne ne circulant pas dans un sens
+    int metro_parisien_sans_4[SIZE][SIZE] = {
+        {0,   INF, INF, INF, INF, 12,  5},
+        {INF, 0,   2,   INF, INF, INF, 7},
+        {INF, 2,   0,   13,  INF, INF, INF},
+        {INF, INF, 13,  0,   INF, INF, INF},
+        {INF, INF, INF, INF, 0,   INF, 2},
+        {12,  INF, INF, INF, INF, 0,   12},
+        {5,   7,   10,  INF, 2,   INF, 0}
+    };
 
-    /* demander le noeud de départ */
+
+    int **matrice = NULL; //matrice de taille [size][size]
+    
+    // allocation mémoire
+    matrice = malloc(SIZE * sizeof(int*));
+    for (int i = 0; i < SIZE; i++)
+        matrice[i] = malloc(SIZE * sizeof(int*));
+
+    // remplissage avec une matrice déjà existante
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++) {
+            if (argv[1][0] == '1')
+                matrice[i][j] = metro_parisien[i][j];
+            else if (argv[1][0] == '2')
+                matrice[i][j] = metro_parisien_sans_4[i][j];
+        }
+    }
+
+
+
+    // demande du noeud de départ
     int node = 1;
-    printf("Quel noeud de départ ? (entre 1 et 7)\n>");
-    scanf("%d", &node);
+    printf("Quel noeud de départ ? (entre 1 et %d)\n>", SIZE);
+    while(1) {
+        scanf("%d", &node);
+        if (node >= 1 && node <= SIZE)
+            break;
+        printf("wrong node (entre 1 et %d)\n>", SIZE);
+    }
 
-    /* afficher la distance minimal, puis le chemin à parcourir
-     * entre le départ et chacun des autres noeud
-     */
 
     //trajets le plus rapide entre "node" et tous les autres
-    int paths[SIZE][SIZE+1]; //+1 pour laisser la place à la distance du trajet
-
-    int **fp = malloc(SIZE * sizeof(int*));
+    int **paths = NULL; //doit être de taille [size][size+1]
+    
+    //allocation mémoire
+    paths = malloc(SIZE * sizeof(int*));
     for (int i = 0; i < SIZE+1; i++)
-        fp[i] = malloc(SIZE+1 * sizeof(int*));
+        paths[i] = malloc(SIZE+1 * sizeof(int*));
 
+    //remplissage
     for (int i = 0; i < SIZE; i++){
         for (int j = 0; j < SIZE+1; j++)
-            fp[i][j] = -1;
+            paths[i][j] = -1;
     }
 
-    dijkstra(mat, SIZE, node-1, fp);
 
 
-    for (int i = 0; i < SIZE; i++) {
-        printf("fp[%d] = ", i);
-        print_tab(fp[i], SIZE+1);
-    }
+
+    //résolution des trajets
+    dijkstra(matrice, SIZE, node, paths);
+
+    printf("\nNoeud de départ : %d\n\n", node);
+    afficher_trajets(paths, SIZE, node);
 
     return 0;
 }
-
-
